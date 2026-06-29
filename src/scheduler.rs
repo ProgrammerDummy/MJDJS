970dc29cd80c0208963148b511017fb0cf837e26
ProgrammerDummy <mangjee.24@gmail.com>
2026-06-28 21:11:59 -0400

    basic scheduler functions and simulated execution made

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,9 +1,8 @@
-use crate::job_data_structures::{Job, JobQueue, QueueError};
+use crate::job_data_structures::{Job, JobQueue};
 use crate::worker::{WorkerPool};
-use std::hash::Hash;
+use crate::state_machine::{transition, determine_next_event, JobEvent};
 use std::sync::{Arc, Mutex};
 use tokio::sync::mpsc::{Receiver, Sender, channel};
-use rand::Rng;
 use std::collections::HashMap;
 
 pub enum JobOutcome {
@@ -26,7 +25,7 @@ pub struct Scheduler {
     pub worker_pool: Arc<Mutex<WorkerPool>>,
     pub job_results: Receiver<JobResult>, //wil receive JobResult from mpsc channel
     pub sender: Sender<JobResult>,
-    pub in_flight: HashMap<u64, Job>, //since im dequeuing jobs, i need to store them elsewhere
+    pub in_flight: HashMap<u64, Job>, //represents jobs currently managed by scheduler
 }
 
 impl Scheduler {
@@ -48,11 +47,47 @@ impl Scheduler {
                 Some(msg) = self.job_results.recv() => {
                     match msg.event {
                         JobOutcome::Success {result} => {
+                            match self.in_flight.get_mut(&msg.job_id) {
+                                Some(job) => {
+                                    let _ = transition(job, JobEvent::Success { completed_at: 0u64, result}); //explicit ignore and placeholder values until i implement real clock
+                                    self.in_flight.remove(&msg.job_id); //remove the job from the in_flight hashmap
+                                },
 
+                                None => {},
+                            }
                         },
 
                         JobOutcome::Failure {error} => {
-                            
+                            match self.in_flight.get_mut(&msg.job_id) {
+                                Some(job) => {
+                                    let _ = transition(job, JobEvent::Fail {error}); //another placeholder
+
+                                    match determine_next_event(job) {
+                                        JobEvent::Retry {retry_at} => {
+                                            let _ = transition(job, JobEvent::Retry { retry_at });
+                                            {
+                                                let mut queue_lock = self.queue.lock().unwrap();
+                                                queue_lock.enqueue(job.clone()); 
+                                            }
+                                        },
+
+                                        JobEvent::DeadLetter {reason} => {
+                                            let _ = transition(job, JobEvent::DeadLetter { reason });
+                                            self.in_flight.remove(&msg.job_id);
+                                        },
+
+                                        _ => {}, //since determine_next_event only ever returns deadletter or retry, im ignoring this
+                                    }
+                                }
+
+                                None => {},
+                            }
+
+                            {
+                                //regardless of outcome, free the worker
+                                let mut worker_pool_lock = self.worker_pool.lock().unwrap();
+                                worker_pool_lock.free_worker(msg.worker_id);
+                            }
                         }
                     }
                 },
@@ -76,6 +111,16 @@ impl Scheduler {
                                     let tx_clone = self.sender.clone();
                                     let worker_id_clone = worker_id.clone();
                                     let job_clone = job.clone();
+
+                                    {
+                                        let mut worker_pool_lock = self.worker_pool.lock().unwrap();
+                                        if worker_pool_lock.assign_job(worker_id, job.id.clone()) != Ok(()) {
+                                            return;
+                                        }
+                                    }
+
+                                    self.in_flight.insert(job.id, job);
+                                    
                                     tokio::spawn(async move {
                                         simulate_job_execution(job_clone, worker_id_clone, tx_clone).await;
                                     });
@@ -88,7 +133,7 @@ impl Scheduler {
                         None => return,
                     }
                 } => {},
-                //other branch for steps 1-3 go here?
+
 
             } 
         }
@@ -102,7 +147,7 @@ impl Scheduler {
 async fn simulate_job_execution(job: Job, worker_id: u64, sender: Sender<JobResult>) { //just a simulator for executing jobs
     let exec_time: u64 = 100;
     tokio::time::sleep(std::time::Duration::from_millis(exec_time)).await;
-    sender.send(JobResult {
+    let _ = sender.send(JobResult {
         job_id: job.id,
         worker_id,
         event: JobOutcome::Success { result: 1 },
