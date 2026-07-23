e005475d32d0707e71d30fc4b35a4c4f4a02c4a7
ProgrammerDummy <mangjee.24@gmail.com>
2026-07-23 16:16:53 -0400

    making main server operation

--- scheduler-server/src/main.rs
+++ scheduler-server/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+mod service;
+use scheduler_core::proto::scheduler_service_server::SchedulerServiceServer;
+
+use crate::service::MySchedulerService;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = std::env::var("SCHEDULER_ADDR")
+        .unwrap_or_else(|_| "127.0.0.1:50051".to_string())
+        .parse()?;
+
+    let service = MySchedulerService::new();
+
+    println!("scheduler-server listening on {addr}");
+
+    tonic::transport::Server::builder()
+        .add_service(SchedulerServiceServer::new(service))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
