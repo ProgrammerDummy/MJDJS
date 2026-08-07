987d8017b5d068a31210925b986b16e6cfd2de7d
Matthew Jeong <mangjee.24@gmail.com>
2026-08-07 19:05:59 -0400

    removed current_job_count

--- scheduler-server/src/worker.rs
+++ scheduler-server/src/worker.rs
@@ -10,7 +10,6 @@ pub struct WorkerInfo {
     hostname: String,
     job_types_supported: Vec<String>,
     max_concurrent_jobs: u32,
-    current_job_count: AtomicU32,
     assigned_jobs: HashSet<uuid::Uuid>,
     last_heartbeat_at: Instant,
     tags: HashMap<String, String>,
