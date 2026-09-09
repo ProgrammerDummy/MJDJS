fcc9e483adbc04e47a660a5e27865fadf48c8c34
ProgrammerDummy <mangjee.24@gmail.com>
2026-09-08 23:36:09 -0400

    added large_enum_variant to be allowed to pass clippy warnings

--- scheduler-core/build.rs
+++ scheduler-core/build.rs
@@ -1,4 +1,9 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_prost_build::compile_protos("proto/scheduler.proto")?;
+    tonic_prost_build::configure()
+        .type_attribute(
+            "scheduler.RequestWorkResponse.result",
+            "#[allow(clippy::large_enum_variant)]",
+        )
+        .compile_protos(&["proto/scheduler.proto"], &["proto"])?;
     Ok(())
 }
